1b2bb3b2cdea4d54a4bb3d6d20f11f2cb7397c22
D N <127353677+Nejwid@users.noreply.github.com>
2025-01-28 20:19:34 +0100

    Update factory.h

--- factory.h
+++ factory.h
@@ -3,68 +3,56 @@
 #include <iostream>
 
 
-namespace fabryka
-{
+namespace fabryka{
 	using namespace::std;
 	
-	class Produkt
-	{
+	class Produkt{
 	public:
 		Produkt(){}
 		virtual void info() const = 0;
 		virtual ~Produkt(){}
 	};
 
-	class ProduktA : public Produkt
-	{
+	class ProduktA : public Produkt{
 	public:
 		ProduktA():Produkt(){}
-		void info() const override
-		{
+		void info() const override{
 			cout << " this is produkt A " << endl;
 		}
 	};
 
-	class ProduktB : public Produkt
-	{
+	class ProduktB : public Produkt{
 	public:
 		ProduktB() :Produkt() {}
-		void info() const override
-		{
+		void info() const override{
 			cout << " this is produkt B " << endl;
 		}
 	};
 
-	class Fabryka
-	{
+	class Fabryka{
 	public:
 		virtual unique_ptr<Produkt> factor() = 0;
 		virtual ~Fabryka(){}
 	};
 
-	class FabrykaA : public Fabryka
-	{
+	class FabrykaA : public Fabryka{
 	public:
 		FabrykaA():Fabryka(){}
-		unique_ptr<Produkt> factor() override
-		{
+		unique_ptr<Produkt> factor() override{
 			return make_unique<ProduktA>();
 		}
 	};
 
-	class FabrykaB : public Fabryka
-	{
+	class FabrykaB : public Fabryka{
 	public:
 		FabrykaB() :Fabryka() {}
-		unique_ptr<Produkt> factor() override
-		{
+		unique_ptr<Produkt> factor() override{
 			return make_unique<ProduktB>();
 		}
 	};
 
 
-	void test()
-	{
+	void test(){
 		unique_ptr<Fabryka> fa = make_unique<FabrykaA>();
 		unique_ptr<Fabryka> fb = make_unique<FabrykaB>();
 		unique_ptr<Produkt> a = fa->factor();
