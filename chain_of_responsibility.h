7c6bcee059076a074711a1857e727f30325f995f
D N <127353677+Nejwid@users.noreply.github.com>
2025-01-29 18:23:50 +0100

    Update chain_of_responsibility.h

--- chain_of_responsibility.h
+++ chain_of_responsibility.h
@@ -2,74 +2,8 @@
 #include <iostream>
 #include <memory>
 
-namespace chainOfR
-{
-	string resetHas³a = "R", softConfig = "S", awariaSprzêtu = "A";
-
-	class CC{
-	protected:
-		unique_ptr<CC> nextCC;
-	public:
-		void setCC(unique_ptr<CC> next){
-			nextCC = move(next);
-		}
-		virtual void help(const string & task){
-			if (nextCC)
-				nextCC->help(task);
-			else
-				cout << "cannot help";
-		}
-		virtual ~CC(){}
-	};
-
-	class BasicCC : public CC{
-	public:
-		void help(const string& task) override{
-			if (task == resetHas³a){
-				cout << "password reseted";
-			}
-			else{
-				CC::help(task);
-			}
-		}
-	};
-
-	class AdvancedCC : public CC{
-	public:
-		void help(const string& task) override{
-			if (task == softConfig)
-				cout << "soft config done";
-			else
-				CC::help(task);
-		}
-	};
-
-	class SpecialistCC : public CC{
-	public:
-		void help(const string& task) override{
-			if (task == awariaSprzêtu)
-				cout << "device fixed";
-			else
-				CC::help(task);
-		}
-	};
-
-
-	void test()
-	{
-		unique_ptr<BasicCC> b = make_unique<BasicCC>();
-		unique_ptr<AdvancedCC> a = make_unique<AdvancedCC>();
-		unique_ptr<SpecialistCC> s = make_unique<SpecialistCC>();
-		a->setCC(move(s));
-		b->setCC(move(a)); // TUTAJ NAJWAZNIEJSZY MOMENT - trzeba ustawic wskazniki od góry s->a, a(s)->b = b(a(s))
-		b->help("A");
-		b->help("S");
-		b->help("X");
-	}
-
-}
-
-namespace chainOfR_test{
+// symulacja Customer Care
+namespace chainOfResponsibility {
 	using namespace::std;
 
 	class Ticket{public: virtual ~Ticket(){} }; class Basic : public Ticket{}; class Advanced : public Ticket{};
@@ -110,6 +44,7 @@ namespace chainOfR_test{
 		}
 	};
 
+	// basic instancja pomoÅ¼e lub przeniesie ticket do instancji advanced 
 	class CustomerCare{
 	private:
 		unique_ptr<BasicCC> basic;
