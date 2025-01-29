0732b635e6269cabf1c776b3342fdf5c971c90da
D N <127353677+Nejwid@users.noreply.github.com>
2025-01-29 23:23:18 +0100

    Update and rename BridgeY.h to Bridge.h

--- Bridge.h
+++ Bridge.h
@@ -1,11 +1,42 @@
 #pragma once
 #include <memory>
 #include <list>
-#include "BridgeX.h"
+#include <string>
 
 namespace bridge_Shelter {
     using namespace std;
 
+    class Animal {
+    protected:
+        string name;
+        int age;
+    public:
+        Animal(string s, int n);
+        virtual void speak() const = 0;
+        void displayInfo() const;
+        int getAge() const;
+        bool operator<(const Animal& other);
+        virtual ~Animal();
+    };
+
+    class Dog : public Animal {
+    public:
+        Dog(string s, int n);
+        void speak() const override;
+    };
+
+    class Cat : public Animal {
+    public:
+        Cat(string s, int n);
+        void speak() const override;
+    };
+
+    class Bird : public Animal {
+    public:
+        Bird(string s, int n);
+        void speak() const override;
+    };
+//-----------------------------------------------//
     class Shelter {
     private:
         list<unique_ptr<Animal>> animals;
