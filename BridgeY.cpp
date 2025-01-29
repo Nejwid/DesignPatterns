6d46e69afc4fbb7a2ca0df1f7212409e758f376d
D N <127353677+Nejwid@users.noreply.github.com>
2025-01-29 23:24:55 +0100

    Update BridgeY.cpp

--- BridgeY.cpp
+++ BridgeY.cpp
@@ -1,7 +1,40 @@
-#include "BridgeY.h"
+#include "Bridge.h"
 
 namespace bridge_Shelter {
+    Animal::Animal(string s, int n) : name(s), age(n) {}
 
+    void Animal::displayInfo() const {
+        cout << name << ", " << age << " ";
+    }
+
+    int Animal::getAge() const {
+        return age;
+    }
+
+    bool Animal::operator<(const Animal& other) {
+        return age < other.age;
+    }
+
+    Animal::~Animal() {}
+
+    Dog::Dog(string s, int n) : Animal(s, n) {}
+
+    void Dog::speak() const {
+        cout << "wooof" << endl;
+    }
+
+    Cat::Cat(string s, int n) : Animal(s, n) {}
+
+    void Cat::speak() const {
+        cout << "meoow" << endl;
+    }
+
+    Bird::Bird(string s, int n) : Animal(s, n) {}
+
+    void Bird::speak() const {
+        cout << "tweet" << endl;
+    }
+//=============================================================//
     void Shelter::addDog(string s, int n) {
         animals.emplace_back(make_unique<Dog>(s, n));
     }
